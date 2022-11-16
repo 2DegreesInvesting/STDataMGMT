fe29f58d5b747f7449c03c4e240fe4ba451ef27a
frmfisc <110397916+frmfisc@users.noreply.github.com>
2022-11-16 21:04:34 +0100

    added one data cleaning step renaming Central&SouthAmerica to CentralandSouthAmerica

--- data-raw/run_prepare_weo2021_manually_added_Scenarios_AnalysisInput_start_year.R
+++ data-raw/run_prepare_weo2021_manually_added_Scenarios_AnalysisInput_start_year.R
@@ -3,17 +3,17 @@ devtools::load_all()
 ##ok. we need to add weo_2021 manually that is the output of the PACTA routine with start_year == 2021 
 ##it is not pretty but no other way around 
 
-# scenario_input_p4i_weo2021 <- read.csv
-# ("~/Dropbox (2° Investing)/PortCheck/00_Data/01_ProcessedData/03_ScenarioData/scenario_input_p4i_weo2021.csv")
-# 
-# Scenarios_AnalysisInput_2021 <- read.csv
-# ("~/Dropbox (2° Investing)/PortCheck/00_Data/01_ProcessedData/03_ScenarioData/Scenarios_AnalysisInput_2021.csv")
-# 
-# weo2021_manually_added_Scenarios_AnalysisInput_2021 <- rbind(scenario_input_p4i_weo2021, Scenarios_AnalysisInput_2021)
-# 
-# write.csv(weo2021_manually_added_Scenarios_AnalysisInput_2021,
-#           "~/Dropbox (2° Investing)/PortCheck/00_Data/01_ProcessedData/03_ScenarioData/
-#           weo2021_manually_added_Scenarios_AnalysisInput_2021.csv", row.names = FALSE)
+scenario_input_p4i_weo2021 <- read.csv("~/Dropbox (2° Investing)/PortCheck/00_Data/01_ProcessedData/03_ScenarioData/scenario_input_p4i_weo2021.csv")
+
+Scenarios_AnalysisInput_2021 <- read.csv("~/Dropbox (2° Investing)/PortCheck/00_Data/01_ProcessedData/03_ScenarioData/Scenarios_AnalysisInput_2021.csv")
+
+weo2021_manually_added_Scenarios_AnalysisInput_2021 <- rbind(scenario_input_p4i_weo2021, Scenarios_AnalysisInput_2021)
+
+weo2021_manually_added_Scenarios_AnalysisInput_2021$ScenarioGeography <- str_replace(weo2021_manually_added_Scenarios_AnalysisInput_2021$ScenarioGeography, "Central&SouthAmerica", "CentralandSouthAmerica")
+
+
+write.csv(weo2021_manually_added_Scenarios_AnalysisInput_2021,
+          "~/Dropbox (2° Investing)/PortCheck/00_Data/01_ProcessedData/03_ScenarioData/weo2021_manually_added_Scenarios_AnalysisInput_2021.csv", row.names = FALSE)
 
 
 
