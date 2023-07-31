687e0642bb15f146bfef2f972658e1170e736b22
bertrand <bertrand.gallice.pro@gmail.com>
2023-07-31 12:09:37 +0200

    update: new script to prepare financial data, using oxford data as the source

--- R/prepare_prewrangled_financial_data_stress_test.R
+++ R/prepare_prewrangled_financial_data_stress_test.R
@@ -1,88 +1,152 @@
-#' Prewrangle the prepared eikon data into the format used by the stress test
+#' Create averages of eikon variables for companies with missing values
 #'
-#' @param eikon_data A data frame which contains the prepared eikon data as
-#'   provided after running prepare_eikon_data()
+#' @description This function uses the median for averaging numeric variables
+#' because the results may otherwise be very sensitive to outliers. Averages are
+#' created only based on values we previously obtained directly from eikon.
+#' @param data A data frame holding processed and possibly filtered Eikon data
+#' @param minimum_sample_size A numeric vector of length one that determines the
+#'   minimum required absolute sample size of the given subgroup. Below this
+#'   value we do not calculate an average for this sub group.
+#' @param minimum_ratio_sample A numeric vector of length one that determines
+#'   the minimum required ratio of sub group to overall sample. Below this
+#'   value we do not calculate an average for this sub group.
+#' @param allowed_range_npm A numeric vector indicating the allowed minimum and
+#'   maximum values for the net profit margin
 #'
-#' @return A list of three data.frames
-prepare_prewrangled_financial_data_stress_test <- function(eikon_data) {
-  # ... ADO 2563 - temporarily remove all production related data from master data
-  # in a next version, we will simply split financial data and production data in
-  # two separate, but closely related files.
-  # for now, we use distinct_all after removing the production data and check there
-  # are no duplicates
-  # ... aggregate to the ticker/company level
-  financial_data_stress_test <- eikon_data %>%
-    dplyr::select(
-      .data$company_name, .data$company_id, .data$corporate_bond_ticker,
-      .data$final_pd, .data$final_profit_margin_preferred,
-      .data$final_profit_margin_unpreferred, .data$final_leverage_s_avg,
-      .data$final_asset_volatility_s_avg
-    )
-
-  names(financial_data_stress_test) <- names(financial_data_stress_test) %>%
-    stringr::str_remove_all("final_")
-
-  financial_data_stress_test <- financial_data_stress_test %>%
-    dplyr::distinct_all() %>%
-    dplyr::mutate(net_profit_margin = .data$profit_margin_preferred) %>%
-    # TODO: logic unclear thus far
-    dplyr::mutate(
-      net_profit_margin = dplyr::case_when(
-        .data$net_profit_margin < 0 &
-          dplyr::between(.data$profit_margin_unpreferred, 0, 1) ~
-          .data$profit_margin_unpreferred,
-        .data$net_profit_margin < 0 & .data$profit_margin_unpreferred < 0 ~ 0,
-        .data$net_profit_margin < 0 & .data$profit_margin_unpreferred > 1 ~ 0,
-        .data$net_profit_margin > 1 &
-          dplyr::between(.data$profit_margin_unpreferred, 0, 1) ~
-          .data$profit_margin_unpreferred,
-        .data$net_profit_margin > 1 & .data$profit_margin_unpreferred > 1 ~ 1,
-        .data$net_profit_margin > 1 & .data$profit_margin_unpreferred < 0 ~ 1,
-        TRUE ~ .data$net_profit_margin
-      )
-    )
-
-  financial_data_stress_test_rm_profit_margin <- financial_data_stress_test %>%
-    dplyr::filter(.data$net_profit_margin <= 0) %>%
-    dplyr::select(
-      .data$company_name, .data$company_id, .data$corporate_bond_ticker,
-      .data$net_profit_margin, .data$profit_margin_unpreferred
-    )
-
-  financial_data_stress_test <- financial_data_stress_test %>%
-    dplyr::filter(.data$net_profit_margin > 0) %>%
-    dplyr::rename(
-      debt_equity_ratio = .data$leverage_s_avg,
-      volatility = .data$asset_volatility_s_avg
+#' @return A data frame
+#' @export
+create_averages_eikon_sector <- function(data,
+                                         minimum_sample_size,
+                                         minimum_ratio_sample,
+                                         allowed_range_npm) {
+  eikon_trbc_averages <- data %>%
+    dplyr::group_by(.data$trbc_industry_name) %>%
+    dplyr::mutate(size_subgroup = dplyr::n()) %>%
+    dplyr::summarise(
+      size_subgroup = mean(.data$size_subgroup, na.rm = TRUE),
+      size_sample = dplyr::n(),
+      ratio_sample_subgroup = .data$size_sample / .data$size_subgroup,
+      sample_sufficient = dplyr::if_else(.data$size_sample > .env$minimum_sample_size, TRUE, FALSE),
+      ratio_sufficient = dplyr::if_else(
+        .data$ratio_sample_subgroup > .env$minimum_ratio_sample,
+        TRUE,
+        FALSE
+      ),
+      avg_pd = stats::median(.data$pd, na.rm = TRUE),
+      avg_volatility = stats::median(.data$volatility, na.rm = T),
+      avg_net_profit_margin = stats::median(.data$net_profit_margin, na.rm = T),
+      avg_debt_equity_ratio = stats::median(.data$debt_equity_ratio, na.rm = T)
     ) %>%
-    dplyr::select(
-      .data$company_name, .data$company_id, .data$corporate_bond_ticker,
-      .data$pd, .data$net_profit_margin, .data$debt_equity_ratio,
-      .data$volatility
-    )
-
-  financial_data_stress_test_rm_profit_margin <- financial_data_stress_test_rm_profit_margin %>%
-    dplyr::filter(.data$net_profit_margin <= 0) %>%
-    dplyr::select(
-      .data$company_name, .data$company_id, .data$corporate_bond_ticker,
-      .data$net_profit_margin, .data$profit_margin_unpreferred
-    )
-
-  financial_data_stress_test_rm_non_ascii <- financial_data_stress_test %>%
-    dplyr::mutate(enc = stringi::stri_enc_mark(.data$company_name)) %>%
-    dplyr::filter(.data$enc != "ASCII") %>%
-    dplyr::select(-.data$enc)
-
-  financial_data_stress_test <- financial_data_stress_test %>%
-    dplyr::mutate(enc = stringi::stri_enc_mark(.data$company_name)) %>%
-    dplyr::filter(.data$enc == "ASCII") %>%
-    dplyr::select(-.data$enc)
-
-  # TODO: any logic/bounds needed for debt/equity ratio and volatility?
-
-  list_prewrangled_financial_data <- list(
-    financial_data_stress_test,
-    financial_data_stress_test_rm_profit_margin,
-    financial_data_stress_test_rm_non_ascii
-  )
+    dplyr::ungroup() %>%
+    dplyr::filter(dplyr::across(dplyr::contains("avg_"), ~ (!is.na(.))),
+                  dplyr::across(dplyr::contains("avg_"), ~ (!is.infinite(.)))) %>%
+    dplyr::filter(.data$sample_sufficient == TRUE |
+                    .data$ratio_sufficient == TRUE) %>%
+    dplyr::filter(dplyr::between(
+      .data$avg_net_profit_margin,
+      min(.env$allowed_range_npm, na.rm = TRUE),
+      max(.env$allowed_range_npm, na.rm = TRUE)
+    ))
+  return(eikon_trbc_averages)
 }
+
+#' Creates the averages to fill missing values where no match is found between abcd and eikon
+#' 1. join eikon and abcd to extract companies matched
+#' 2. count the number of ald_sector/technology pair for each eikon trbc industry
+#' 3. join the `eikon_trbc_averages` to get the financial replacement values for
+#'    ald_sector/technology pairs depending with which eikon trbc they match
+#' 4. compute the median of financial variables over each ald_sector/technology pair.
+#'    This median is ponderated by the number of companies in abcd that have been matched
+#'    with the eikon trbc, since the `eikon_trbc_averages` are computed for each trbc.
+#'
+#' @export
+create_averages_abcd_sectors <-
+  function(eikon_data,
+           abcd_data,
+           eikon_trbc_averages) {
+    matched_companies <-
+      eikon_data %>% dplyr::inner_join(abcd_data %>% dplyr::distinct(company_id, ald_sector, technology),
+                                       by = "company_id")
+
+    # # subgroups volume check debug
+    # matched_companies %>%
+    #   group_by(trbc_industry_name, ald_sector, technology) %>%
+    #   summarise(nrow = n()) %>%
+    #   arrange(desc(nrow)) %>%
+    #   print(n = 1000)
+
+    abcd_sectors_averages <- matched_companies %>%
+      dplyr::group_by(.data$trbc_industry_name,
+                      .data$ald_sector,
+                      .data$technology) %>%
+      dplyr::summarise(ngroup_match = dplyr::n()) %>%
+      dplyr::left_join(
+        eikon_trbc_averages %>% dplyr::select(
+          .data$trbc_industry_name,
+          .data$avg_pd,
+          .data$avg_volatility,
+          .data$avg_net_profit_margin,
+          .data$avg_debt_equity_ratio
+        ),
+        by = "trbc_industry_name"
+      ) %>%
+      dplyr::group_by(ald_sector, technology) %>%
+      dplyr::summarise(
+        avg_pd = spatstat.geom::weighted.median(.data$avg_pd, w = .data$ngroup_match, na.rm = T),
+        avg_volatility = spatstat.geom::weighted.median(
+          .data$avg_volatility,
+          w = .data$ngroup_match,
+          na.rm = T
+        ),
+        avg_net_profit_margin = spatstat.geom::weighted.median(
+          .data$avg_net_profit_margin,
+          w = .data$ngroup_match,
+          na.rm = T
+        ),
+        avg_debt_equity_ratio = spatstat.geom::weighted.median(
+          .data$avg_debt_equity_ratio,
+          w = .data$ngroup_match,
+          na.rm = T
+        )
+      )
+
+    # check values coherence:
+    # subgroup_averages %>% filter(grepl("Electric", trbc_industry_name)) %>% print(width=Inf)
+    # subgroup_averages %>% filter(grepl("Oil", trbc_industry_name)) %>% print(width=Inf)
+    # ==> Oil PD < Electric PD like in eikon_trbc_averages
+
+    return(abcd_sectors_averages)
+  }
+
+#' infer values on abcd where eikon does not match
+#' companies are matched to the `abcd_sectors_averages` on their sector/tech pairs
+#' financial variables are average for company with multiple sector/tech
+#'
+#' @export
+expand_eikon_on_abcd <-
+  function(eikon_data,
+           abcd_data,
+           abcd_sectors_averages) {
+    matched_companies <-
+      eikon_data %>% dplyr::inner_join(abcd_data %>% dplyr::distinct(.data$company_id),
+                                       by = "company_id")
+    not_matched_companies <- abcd_data %>%
+      dplyr::filter(!.data$company_id %in% matched_companies[["company_id"]])
+
+    inferred_companies <- not_matched_companies %>%
+      dplyr::distinct(.data$company_id, .data$ald_sector, .data$technology) %>%
+      dplyr::left_join(abcd_sectors_averages, by = c("ald_sector", "technology")) %>%
+      dplyr::group_by(.data$company_id) %>%
+      dplyr::summarise(
+        debt_equity_ratio = mean(.data$avg_debt_equity_ratio, na.rm = T),
+        pd = mean(.data$avg_pd, na.rm = T),
+        net_profit_margin = mean(.data$avg_net_profit_margin, na.rm =
+                                   T),
+        volatility = mean(.data$avg_volatility, na.rm = T)
+      )
+
+    eikon_full <-
+      dplyr::bind_rows(matched_companies, inferred_companies)
+
+    return(eikon_full)
+  }
--- data-raw/run_prepare_prewrangled_financial_data_stress_test.R
+++ data-raw/run_prepare_prewrangled_financial_data_stress_test.R
@@ -1,24 +1,63 @@
 devtools::load_all()
 
-# requires eikon_data
-# this can be obtained by running run_prepare_eikon_data
-# without saving the output file
-
-# prepare financial data stress test--------------------------------
-list_prewrangled_financial_data <- eikon_data %>%
-  prepare_prewrangled_financial_data_stress_test()
-
-list_prewrangled_financial_data[[1]] %>%
-  readr::write_csv(
-    file.path("data-raw", "prewrangled_financial_data_stress_test.csv")
+eikon_data <-
+  readr::read_csv(
+    r2dii.utils::path_dropbox_2dii("ST_INPUTS", "ST_PRODUCTION", "eikon_data_enhanced_ids.csv")
+  )
+abcd_data <-
+  readr::read_csv(
+    r2dii.utils::path_dropbox_2dii("ST_INPUTS", "ST_PRODUCTION", "abcd_stress_test_input.csv")
   )
 
-# optional - exporting qa files for removed profit margins
-# path_fin_data_st_rm_npm <- file.path(output_path_db_analysis_inputs, "financial_data_stress_test_rm_npm.csv")
-# list_prewrangled_financial_data[[2]] %>%
-#   readr::write_csv(path_fin_data_st_rm_npm)
+eikon_data <- eikon_data %>%
+  dplyr::rename(
+    debt_equity_ratio = credit_structural_leverage,
+    # TODO OK ?
+    corporate_bond_ticker = ticker_symbol,
+    pd = `credit_structural_pd_%`,
+    net_profit_margin = `net_profit_margin_%`,
+    volatility = `credit_structural_asset_volatility_%`
+  ) %>%
+  dplyr::mutate(
+    net_profit_margin = net_profit_margin / 100,
+    volatility = volatility / 100,
+    pd = pd / 100
+  ) %>%
+  dplyr::select(
+    company_id,
+    trbc_industry_name,
+    corporate_bond_ticker,
+    company_name,
+    debt_equity_ratio,
+    pd,
+    net_profit_margin,
+    volatility
+  )
 
-# optional - exporting qa files for ascii character
-# financial_data_stress_test_rm_non_ascii <- file.path(output_path_db_analysis_inputs, "financial_data_stress_test_rm_non_ascii.csv")
-# list_prewrangled_financial_data[[3]] %>%
-#   readr::write_csv(financial_data_stress_test_rm_non_ascii)
+
+# parameters for minimum requirements to reference subgroups in creating averages
+# determine size of subgroup below which we do not use the average because the
+# minimum required sample size of reference subgroup
+n_min_sample <- 50
+# minimum required ratio of reference subgroup to sample
+min_ratio_sample_subgroup <- 1 / 3
+# cut off values for profit margins
+range_profit_margin <- c(-Inf, Inf)
+eikon_trbc_averages <- create_averages_eikon_sector(
+  eikon_data,
+  minimum_sample_size = n_min_sample,
+  minimum_ratio_sample = min_ratio_sample_subgroup,
+  allowed_range_npm = range_profit_margin
+)
+
+abcd_sectors_averages <-
+  create_averages_abcd_sectors(eikon_data, abcd_data, eikon_trbc_averages)
+
+eikon_full <-
+  expand_eikon_on_abcd(eikon_data, abcd_data, abcd_sectors_averages)
+
+eikon_full <- eikon_full %>%
+  dplyr::select(c(-.data$trbc_industry_name)) %>%
+  dplyr::left_join(abcd_data %>% distinct(.data$company_id, .data$company_name))
+
+eikon_full %>% readr::write_csv(file.path("data-raw", "prewrangled_financial_data_stress_test.csv"))
