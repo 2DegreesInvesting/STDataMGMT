35419a7b4b7ed0839b78a5ce6a04c01992530bda
frmfisc <110397916+frmfisc@users.noreply.github.com>
2023-08-19 12:10:13 +0200

    styler

--- R/prepare_LCOE_adjusted_price_data.R
+++ R/prepare_LCOE_adjusted_price_data.R
@@ -80,7 +80,7 @@ prepare_lcoe_adjusted_price_data_weo <- function(input_data,
 #' @export
 prepare_lcoe_adjusted_price_data_oxford2021 <- function(input_data_lcoe_oxford,
                                                         average_npm_power, start_year) {
-  #start_year <- 2021
+  # start_year <- 2021
 
   data <- input_data_lcoe_oxford %>%
     dplyr::filter(.data$Sector == "Power") %>%
