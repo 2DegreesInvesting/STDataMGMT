befe993a29480537f646578bbc3003284e36db47
bertrand <bertrand.gallice.pro@gmail.com>
2023-10-26 13:11:45 +0200

    synthetic eikon

--- tests/testthat/fixtures/gen_synthetic_eikon_data.R
+++ tests/testthat/fixtures/gen_synthetic_eikon_data.R
@@ -1,27 +1,47 @@
-devtools::load_all()
 
-ISIN_COUNTRYCODES <- countrycode::codelist %>%
-  dplyr::filter(!is.na(.data$ecb)) %>%
-  dplyr::distinct(ecb) %>%
-  dplyr::pull()
+library(dplyr)
+library(tibble)
 
-create_random_isins <- function(n_assets) {
-  random_isin <- function() {
-    sprintf(
-      "%s%s",
-      paste0(sample(ISIN_COUNTRYCODES, 1, TRUE), collapse = ""),
-      paste0(sample(9, 10, TRUE), collapse = "")
-    )
-  }
-  return(replicate(n_assets, random_isin()))
+# TODO with those columns
+# company_id, company_name, corporate_bond_ticker, pd, net_profit_margin, debt_equity_ratio, volatility
+load("data/company_activities.rda")
+available_companies <- company_activities  %>% dplyr::distinct(company_id) %>% dplyr::pull()
+
+# random sample of companies  
+sample_companies <- sample(available_companies, size=length(available_companies)*10, replace = TRUE)  
+#remove 50% companies at random
+sample_companies <- sample_companies[!(sample_companies %in% 
+  sample(available_companies, size=as.integer(length(available_companies)/2), replace=FALSE)
+  )]
+sample_companies <- tibble(company_id=sample_companies)
+  
+
+random_isin <- function(...) {
+  countries <- countrycode::codelist %>%
+    filter(!is.na(iso2c)) %>%
+    distinct(iso2c) %>%
+    pull(iso2c)
+
+  sprintf(
+    "%s%s",
+    paste0(sample(countries, 1, TRUE), collapse = ""),
+    paste0(sample(9, 10, TRUE), collapse = "")
+  )
 }
 
+random_isins_with_companies <- purrr::map_vec(1:nrow(sample_companies), random_isin)
+random_isins_no_companies <- purrr::map_vec(1:100, random_isin)
+random_isins <- tibble(isin=c(random_isins_with_companies, random_isins_no_companies))
+stopifnot(nrow(random_isins) == length(unique(random_isins$isin))) # check all isin unique
 
-company_ids <- company_activities %>%
-  select(company_id, ald_sector, ald_business_unit, ald_location)
 
+eikon_data <- bind_cols(bind_rows(sample_companies, tibble(company_id=rep(NA, 100))), random_isins) %>%
+  tibble::add_column(
+    # corporate_bond_ticker = NA,
+    pd = runif(nrow(.), min = 0, max = 1),
+    net_profit_margin = runif(nrow(.), min = 0, max = 1),
+    debt_equity_ratio = runif(nrow(.), min = 0, max = 1),
+    volatility = runif(nrow(.), min = 0, max = 1)
+  )
 
-
-# companies_with_isins <- sample(unique(company_ids %>% pull(company_id)), 10, replace=FALSE)
-# companies_with_isins <- tibble::tribble(~isin, )
-# companies_with_isins_at_locations <-
+usethis::use_data(eikon_data, overwrite=TRUE)
