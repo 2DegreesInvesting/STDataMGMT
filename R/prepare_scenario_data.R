8bd67ff8661145803053a6c3eb7c10339f400379
bertrand <bertrand.gallice.pro@gmail.com>
2024-01-23 10:47:26 +0100

    NGFS : filter Non-Biomass Renewables and non-GW Power

--- R/prepare_scenario_data.R
+++ R/prepare_scenario_data.R
@@ -320,6 +320,7 @@ preprepare_ngfs_scenario_data <- function(data, start_year) {
     ) %>%
     dplyr::rename(units = .data$Unit) %>%
     dplyr::ungroup() %>%  ##think it needs to be ungrouped here
+    dplyr::filter(!((.data$sector == "Power") & (.data$units!="GW"))) %>%
     dplyr::select(-c(.data$Model, .data$Variable, .data$Scenario, .data$category_c, .data$category_a, .data$category_b, .data$Region))
 
 
@@ -329,7 +330,7 @@ preprepare_ngfs_scenario_data <- function(data, start_year) {
     dplyr::mutate(value = sum(.data$value)) %>%
     unique()
 
-  delete_renewables <- data %>% dplyr::filter(!.data$technology == "RenewablesCap")
+  delete_renewables <- data %>% dplyr::filter(!.data$technology %in% c("RenewablesCap", "Non-Biomass Renewables"))
 
   data <- dplyr::full_join(combine_renewables_cap, delete_renewables) %>%
     tidyr::unite("scenario", c(.data$model, .data$scenario), sep = "_") %>%
